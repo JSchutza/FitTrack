630ee4a6218bd44112fd392d62d648ac5289c8a4
JSchutza <js.joshua.schutza@gmail.com>
2025-05-08 10:51:41 -0500

    deployment live and working

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// Use MONGO_URI for local development, otherwise use Atlas URI
 dotenv.config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jschutzacluster.vzcgxea.mongodb.net/fittrackdb?retryWrites=true&w=majority&appName=jschutzaCluster`;
 
